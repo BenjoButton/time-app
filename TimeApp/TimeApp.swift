93fcab37658f15862124206993dbff42bb1d8298
Ben <benroberth@live.co.uk>
2021-08-30 11:37:14 +0100

    Initial Commit

---
+++ TimeApp/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  TimeApp
+//
+//  Created by Ben Howland on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
